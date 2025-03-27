47f869586c7182346ad076278c694ca414ac9806
pyrocent <panzieri.ale@gmail.com>
2025-03-27 11:28:05 +0100

    commit

--- src/public/static/js/index.js
+++ src/public/static/js/index.js
@@ -100,7 +100,7 @@ let blue_fra_deck = red_fra_deck = [
 
 gsap.registerPlugin(Draggable);
 Draggable.create("#table *", {
-    bounds: { left: 0 },
+    bounds: {left: 5},
     onClick: function() {
         if (this.target.classList.contains("card")) {
 
