4eeebf3216a58c9196649ba07e7887310e10069d
pyrocent <panzieri.ale@gmail.com>
2025-05-29 17:27:16 +0200

    commit

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,24 @@
 from os import getenv
 from ably import AblyRest
 from fastapi import FastAPI, Form
-from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+from logging import basicConfig, getLogger, Logger, WARNING, INFO
 
-ably: AblyRest = AblyRest(getenv("ABLY_API_KEY"))
+basicConfig(level = INFO, format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+getLogger("ably").setLevel(WARNING)
+getLogger("httpx").setLevel(WARNING)
+getLogger("urllib3").setLevel(WARNING)
+getLogger("uvicorn.error").setLevel(WARNING)
+getLogger("uvicorn.access").setLevel(WARNING)
 
-app: FastAPI = FastAPI()
+fastapi = getLogger("fastapi")
+
+if not (api_key := getenv("ABLY_API_KEY")):
+    raise RuntimeError("❌ ABLY_API_KEY not found")
+
+ably = AblyRest(api_key)
+
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_headers = ["*"],
@@ -14,6 +26,10 @@ app.add_middleware(
     allow_credentials = True,
     allow_origins = ["https://www.playontable.com"]
 )
+
+@app.on_event("startup")
+async def on_startup():
+    fastapi.info("✅ FastAPI server is up and running")
 
 @app.get("/api/auth")
 async def auth():
