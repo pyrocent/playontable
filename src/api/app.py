aabf3fd220d3bc62691a2707a0eca2b5ef96c708
pyrocent <panzieri.ale@gmail.com>
2025-05-29 17:30:05 +0200

    commit

--- src/api/app.py
+++ src/api/app.py
@@ -2,21 +2,10 @@ from os import getenv
 from ably import AblyRest
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-from logging import basicConfig, getLogger, Logger, WARNING, INFO
-
-basicConfig(level = INFO, format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s")
-getLogger("ably").setLevel(WARNING)
-getLogger("httpx").setLevel(WARNING)
-getLogger("urllib3").setLevel(WARNING)
-getLogger("uvicorn.error").setLevel(WARNING)
-getLogger("uvicorn.access").setLevel(WARNING)
-
-fastapi = getLogger("fastapi")
 
 if not (api_key := getenv("ABLY_API_KEY")):
     raise RuntimeError("❌ ABLY_API_KEY not found")
-
-ably = AblyRest(api_key)
+else: ably = AblyRest(api_key)
 
 app = FastAPI()
 app.add_middleware(
@@ -26,10 +15,6 @@ app.add_middleware(
     allow_credentials = True,
     allow_origins = ["https://www.playontable.com"]
 )
-
-@app.on_event("startup")
-async def on_startup():
-    fastapi.info("✅ FastAPI server is up and running")
 
 @app.get("/api/auth")
 async def auth():
