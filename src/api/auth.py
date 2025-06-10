f7e2467e7f74ba2bd9269740ffb9f0a5d5dc2780
pyrocent <panzieri.ale@gmail.com>
2025-06-10 11:52:56 +0200

    commit

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,19 +1,11 @@
 from os import getenv
 from ably import AblyRest
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 if not (api_key := getenv("ABLY_API_KEY")): raise RuntimeError("❌ ABLY_API_KEY not found")
 else: ably = AblyRest(api_key)
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_headers = [],
-    allow_methods = ["GET"],
-    allow_credentials = True,
-    allow_origins = []
-)
 
 @app.get("/api/auth")
 async def auth():
