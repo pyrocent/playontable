201ae2e0c5e32cd9506d81c8e6235d989f96f5b3
pyrocent <panzieri.ale@gmail.com>
2025-06-10 15:22:47 +0200

    commit

--- src/public/static/js/ably.js
+++ src/public/static/js/ably.js
@@ -1,7 +1,7 @@
 import Ably from "https://cdn.jsdelivr.net/npm/ably@2.2.0/+esm";
 
 export class Room {
-    constructor(code) {this.room = new Ably.Realtime({authUrl: "https://www.playontable.com/api/auth"}).channels.get(code);}
+    constructor(code) {this.room = new Ably.Realtime({authUrl: "https://playontable.com/api/auth"}).channels.get(code);}
     on(event, callback) {this.room.subscribe(event, callback);}
     send(event, message = {}) {this.room.publish(event, message);}
 }
