dd5fa037e506d0d6b49bd99a11af3937e2791b15
pyrocent <panzieri.ale@gmail.com>
2025-06-13 17:03:41 +0200

    commit

--- src/web/tools/images.js
+++ src/web/tools/images.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-const axios = require('axios');
+const fs = require("fs");
+const axios = require("axios");
 
 async function urlToBase64(url) {
-    const res = await axios.get(url, {responseType: 'arraybuffer'});
-    const mime = res.headers['content-type'];
-    const base64 = Buffer.from(res.data).toString('base64');
+    const res = await axios.get(url, {responseType: "arraybuffer"});
+    const mime = res.headers["content-type"];
+    const base64 = Buffer.from(res.data).toString("base64");
     return `data:${mime};base64,${base64}`;
 }
 
 (async () => {
-    const html = fs.readFileSync('public/index.html', 'UTF-8');
-    const urls = html.matchAll(/https:\/\/[^\s"']+\.(png|jpg)/gi);
+    let html = fs.readFileSync("public/index.html", "UTF-8");
+    const urls = [...new Set([...html.matchAll(/https:\/\/[^\s"']+\.(png|jpg)/gi)].map(m => m[0]))];
 
-    for (const url of urls) html = html.replace(url, await urlToBase64(url));
+    for (const url of urls) html = html.replaceAll(url, await urlToBase64(url));
 
     fs.writeFileSync("demo.html", html);
 })();
