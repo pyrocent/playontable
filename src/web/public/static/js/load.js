d8d25afff04a15e9cc6355d640cc419e4e3d9d91
pyrocent <panzieri.ale@gmail.com>
2025-06-15 22:42:22 +0200

    commit

---
+++ src/web/public/static/js/load.js
@@ -0,0 +1,34 @@
+import {initRoom} from "./room.js";
+import {startTutorial} from "./tutorial.js";
+import {nanoid} from "https://cdn.jsdelivr.net/npm/nanoid@5.1.5/+esm";
+
+const roomCode = nanoid(5);
+const mainMenu = document.getElementById("main-menu");
+const waitRoom = document.getElementById("wait-room");
+const waitHost = document.getElementById("wait-host");
+const joinCode = document.getElementById("join-code");
+const showCode = document.getElementById("show-code");
+const copyCode = document.getElementById("copy-code");
+
+export function onLoad() {
+    startTutorial(() => mainMenu.showModal());
+
+    mainMenu.addEventListener("close", () => {
+        if (mainMenu.returnValue === "wait-room") {
+            showCode.innerText = roomCode;
+            copyCode.onclick = () => {
+                showCode.innerText = "Copied!";
+                navigator.clipboard.writeText(roomCode);
+                setTimeout(() => showCode.innerText = roomCode, 2000);
+            };
+            waitRoom.showModal();
+        } else if (mainMenu.returnValue === "wait-host") {
+            initRoom(joinCode.value);
+            waitHost.showModal();
+        }
+    });
+
+    waitRoom.addEventListener("close", () => {
+        initRoom(roomCode).send("play");
+    });
+}
