f2dacd29820b6ebb0dc1ef1092eeb0291270960c
pyrocent <panzieri.ale+github@gmail.com>
2025-08-28 16:47:26 +0200

    commit

--- src/web/public/static/js/load.js
+++ src/web/public/static/js/load.js
@@ -4,41 +4,38 @@ import {nanoid} from "https://cdn.jsdelivr.net/npm/nanoid@5.1.5/+esm";
 import {inject} from "https://cdn.jsdelivr.net/npm/@vercel/analytics@1.5.0/+esm";
 import {injectSpeedInsights} from "https://cdn.jsdelivr.net/npm/@vercel/speed-insights@1.2.0/+esm";
 
-export function onLoad() {
+inject();
+injectSpeedInsights();
 
-    inject();
-    injectSpeedInsights();
+const roomCode = nanoid(6);
+const mainMenu = document.getElementById("main-menu");
+const waitRoom = document.getElementById("wait-room");
+const waitHost = document.getElementById("wait-host");
+const joinCode = document.getElementById("join-code");
+const showCode = document.getElementById("show-code");
+const copyCode = document.getElementById("copy-code");
 
-    const roomCode = nanoid(6);
-    const mainMenu = document.getElementById("main-menu");
-    const waitRoom = document.getElementById("wait-room");
-    const waitHost = document.getElementById("wait-host");
-    const joinCode = document.getElementById("join-code");
-    const showCode = document.getElementById("show-code");
-    const copyCode = document.getElementById("copy-code");
+startTutorial(() => {mainMenu.showModal();});
 
-    startTutorial(() => {mainMenu.showModal();});
+mainMenu.addEventListener("close", () => {
+    if (mainMenu.returnValue === "wait-room") {
+        showCode.innerText = roomCode;
+        copyCode.onclick = () => {
+            showCode.innerText = "Copied!";
+            showCode.style.color = "#007649";
+            navigator.clipboard.writeText(roomCode);
+            setTimeout(() => {
+                showCode.innerText = roomCode;
+                showCode.style.color = "#646464";
+            }, 2000);
+        };
+        waitRoom.showModal();
+    } else if (mainMenu.returnValue === "wait-host") {
+        initRoom(joinCode.value);
+        waitHost.showModal();
+    }
+});
 
-    mainMenu.addEventListener("close", () => {
-        if (mainMenu.returnValue === "wait-room") {
-            showCode.innerText = roomCode;
-            copyCode.onclick = () => {
-                showCode.innerText = "Copied!";
-                showCode.style.color = "#007649";
-                navigator.clipboard.writeText(roomCode);
-                setTimeout(() => {
-                    showCode.innerText = roomCode;
-                    showCode.style.color = "#646464";
-                }, 2000);
-            };
-            waitRoom.showModal();
-        } else if (mainMenu.returnValue === "wait-host") {
-            initRoom(joinCode.value);
-            waitHost.showModal();
-        }
-    });
-
-    waitRoom.addEventListener("close", () => {
-        initRoom(roomCode).send("play");
-    });
-}
+waitRoom.addEventListener("close", () => {
+    initRoom(roomCode).send("play");
+});
