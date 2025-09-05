f6b0d8dce759a97abbba10a445b1ae50a6ace37c
pyrocent <panzieri.ale+github@gmail.com>
2025-09-05 10:40:00 +0200

    commit

---
+++ src/web/public/sw.js
@@ -0,0 +1,32 @@
+const URLS_TO_CACHE = ["/"];
+const CACHE_NAME = "playontable-cache";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
