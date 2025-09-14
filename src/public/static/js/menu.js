835669d8730e2462f5ed87d0c93b6d15f6cb18cb
pyrocent <panzieri.ale+github@gmail.com>
2025-09-14 14:11:54 +0200

    commit

--- src/public/static/js/menu.js
+++ src/public/static/js/menu.js
@@ -9,21 +9,32 @@ const joinCode = document.getElementById("join-code");
 const showCode = document.getElementById("show-code");
 const copyCode = document.getElementById("copy-code");
 
-mainMenu.showModal();
+const URLparam = new URLSearchParams(window.location.search);
+const shortcut = URLparam.get("shortcut");
+
+function waitRoomSetup() {
+    showCode.innerText = roomCode;
+    copyCode.onclick = () => {
+        showCode.innerText = "Copied!";
+        showCode.style.color = "#007649";
+        navigator.clipboard.writeText(roomCode);
+        setTimeout(() => {
+            showCode.innerText = roomCode;
+            showCode.style.color = "#646464";
+        }, 2000);
+    };
+    waitRoom.showModal();
+}
+
+if (shortcut === "true") {
+    waitRoomSetup();
+} else {
+    mainMenu.showModal();
+}
 
 mainMenu.addEventListener("close", () => {
     if (mainMenu.returnValue === "wait-room") {
-        showCode.innerText = roomCode;
-        copyCode.onclick = () => {
-            showCode.innerText = "Copied!";
-            showCode.style.color = "#007649";
-            navigator.clipboard.writeText(roomCode);
-            setTimeout(() => {
-                showCode.innerText = roomCode;
-                showCode.style.color = "#646464";
-            }, 2000);
-        };
-        waitRoom.showModal();
+        waitRoomSetup();
     } else if (mainMenu.returnValue === "wait-host") {
         initRoom(joinCode.value);
         waitHost.showModal();
