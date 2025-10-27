99efedfba2c9af53216a44e7735f231a41efd904
pyrocent <panzieri.ale+github@gmail.com>
2025-10-27 17:11:54 +0100

    commit

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
 from api.routers.auth import router as auth
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_methods = ["POST"],
-    allow_origins = ["https://playontable.com"]
-)
 
 app.include_router(auth)
