d5def4ec3a54bfd7f23071fc5997d9cd8074bcc8
pyrocent <panzieri.ale+github@gmail.com>
2025-10-27 17:21:23 +0100

    commit

--- src/api/app.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from api.routers.auth import router as auth
-
-app = FastAPI()
-
-app.include_router(auth)
---
+++ src/api/auth.py
@@ -0,0 +1,18 @@
+from os import getenv
+from ably import AblyRest
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+ably = AblyRest(getenv("ABLY_API_KEY"))
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods = ["POST"],
+    allow_origins = ["https://playontable.com"]
+)
+
+@app.post("/api/auth")
+async def auth():
+    response = await ably.auth.request_token()
+    return response.to_dict()
