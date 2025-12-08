eedff3e612e1465e4bc5778f00ad3a7f4a8f25e6
pyrocent <panzieri.ale+github@gmail.com>
2025-12-08 05:12:30 +0100

    commit

--- src/backend/app.py
+++ src/backend/app.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 rooms = {}
 
-def get_id(length): return "".join(choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(length))
+def get_id(context, length = 5):
+    while (new_id := "".join(choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(length))) in context: pass
+    return new_id
 
 class Room:
     def __init__(self, room_id):
@@ -20,9 +22,9 @@ class Room:
 
 class User:
     def __init__(self, user_room, user_websocket):
-        self.user_id = get_id(5)
         self.user_room = user_room
         self.user_websocket = user_websocket
+        self.user_id = get_id(user_room.room_users)
 
     async def __aenter__(self):
         await self.user_websocket.accept()
@@ -37,14 +39,14 @@ async def handle_message(user_room, user, message): pass
 
 async def handle_websocket(websocket, room_id):
     async with User(rooms.setdefault(room_id, Room(room_id)), websocket) as user:
+        await websocket.send_text(room_id)
         while True:
             await handle_message(user.user_room, user, await websocket.receive_json())
 
 @app.websocket("/websocket")
-async def websocket(websocket):
-    while True:
-        if (room_id := get_id(5)) not in rooms: await handle_websocket(websocket, room_id)
+async def websocket_start_room(websocket):
+    await handle_websocket(websocket, get_id(rooms))
 
 @app.websocket("/websocket/{room_id}")
-async def websocket(websocket, room_id):
+async def websocket_enter_room(websocket, room_id):
     if room_id in rooms: await handle_websocket(websocket, room_id)
