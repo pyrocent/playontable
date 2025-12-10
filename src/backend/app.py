a378c9ae8b3b392e841aad1994af4cfa29b2144e
pyrocent <panzieri.ale+github@gmail.com>
2025-12-10 01:54:43 +0100

    commit

--- src/backend/app.py
+++ src/backend/app.py
@@ -9,9 +9,9 @@ class Room:
     users: list = field(default_factory = list)
     lock: Lock = field(default_factory = Lock, repr = False)
 
-    def __post_init__(self) -> None: rooms[self.id] = self
+    def __post_init__(self): rooms[self.id] = self
 
-    async def broadcast(self, message) -> None:
+    async def broadcast(self, message):
         async with self.lock: users = list(self.users)
         for user in users: await user.websocket.send_text(message)
 
@@ -21,13 +21,13 @@ class User:
     room: Room
     websocket: WebSocket
 
-    async def __aenter__(self) -> User:
+    async def __aenter__(self):
         await self.websocket.accept()
         async with self.room.lock: self.room.users.append(self)
         await self.websocket.send_text(self.room.id)
         return self
 
-    async def __aexit__(self, exc_type, exc, tb) -> None:
+    async def __aexit__(self, exc_type, exc, tb):
         async with self.room.lock: self.room.users.remove(self)
         await self.websocket.close()
         if not self.room.users: rooms.pop(self.room.id, None)
@@ -36,9 +36,9 @@ def get_id(context) -> str:
     while (new_id := "".join(choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(5))) in context: pass
     else: return new_id
 
-async def handle_message(room: Room, user: User, message) -> None: pass
+async def handle_message(room: Room, user: User, message): pass
 
-async def handle_websocket(websocket: WebSocket, room: Room) -> None:
+async def handle_websocket(websocket: WebSocket, room: Room):
     async with room.lock: user_id = get_id([user.id for user in room.users])
     async with User(user_id, room, websocket) as user:
         try:
