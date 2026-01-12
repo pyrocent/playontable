e0f466f71ff09548b5ffb1146a3e815cdd671838
pyrocent <panzieri.ale+github@gmail.com>
2026-01-12 22:58:10 +0100

    commit

--- src/backend/app.py
+++ src/backend/app.py
@@ -2,6 +2,8 @@ from secrets import choice
 from asyncio import gather
 from fastapi import FastAPI, WebSocket
 
+users = {}
+
 class User:
     def __init__(self, websocket, /):
         while users.setdefault(code := "".join(choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(5)), self) is not self: pass
@@ -25,8 +27,6 @@ async def handle(current_user, message = None, /):
     if (hook := message.get("hook")) != "join": await current_user.broadcast(message, exclude = current_user if hook in {"drag", "hand", "fall"} else None)
     elif (host := users.get(message.get("data"))) is not None and host is not current_user:
         for user in (merged := current_user.room | host.room): user.room = merged
-
-users = {}
 
 @(app := FastAPI(openapi_url = None)).websocket("/websocket/")
 async def websocket(websocket: WebSocket):
